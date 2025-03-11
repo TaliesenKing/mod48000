a0ebe5ece4e8c57d84deb6cb293849f711386e39
TaliesenKing <taliesenking@yahoo.com>
2025-03-10 18:15:07 -0700

    about to merge auth-setup into dev

---
+++ backend/db/migrations/20250308034125-create-user.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const { Sequelize } = require('../models');
+
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+};
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING(30)
+      },
+      email: {
+        type: Sequelize.STRING(256),
+        allowNull: false,
+        unique: true
+      },
+      hashedPassword: {
+        type: Sequelize.STRING.BINARY,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    }, options);
+  },
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users';
+    return queryInterface.dropTable(options);
+  }
+};
